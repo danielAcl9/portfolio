1c131d59bbc1aa07faa89e22f5402156478f2508
danielAcl9 <dafeamca99@gmail.com>
2023-01-17 20:07:59 -0500

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,56 +1,51 @@
 import React from "react"
-import imgAbout from "../images/about-img.jpg"
+import imgAbout from "../images/recorte.png"
 import danielCV from "../documents/danielamadocv.pdf"
 import imgDownload from "../images/white-cv.svg"
 
 export default function About(){
     return(
         <div>
-            <h1>----- About -----</h1>
+            <h1 className="about--title" >About me</h1>
 
-            {/* Comentarios para el github, veamos si puedo */}
-            {/* Comentarios para el github, veamos si puedo */}
-            {/* Comentarios para el github, veamos si puedo */}
-            {/* Comentarios para el github, veamos si puedo */}
-            
-            <h1>About me</h1>
+            <div  className="about--container">
+                <img
+                    src={imgAbout}
+                    alt="imagen about"
+                    className="about--img"
+                />
 
-            <img
-                src={imgAbout}
-                alt="imagen about"
-                className="about--img"
-            />
+                <div className="about--text">
+                    <p>I am currently a fourth year Systems Engineering student. 
+                        I have great interest in front-end development, UI Design 
+                        and Data Science 
+                    </p>
+                    <p>Some of the technologies I’ve worked with are:</p>
 
-            <div className="about--text">
-                <p>I am currently a fourth year Systems Engineering student. 
-                    I have great interest in front-end development, UI Design 
-                    and Data Science 
-                </p>
-                <p>Some of the technologies I’ve worked with are:</p>
+                    <div>
+                        <ul className="about--list1">
+                            <li>Python</li>
+                            <li>React.js</li>
+                            <li>Javascript</li>
+                        </ul>
 
-                <div>
-                    <ul className="about--list1">
-                        <li>Python</li>
-                        <li>React.js</li>
-                        <li>Javascript</li>
-                    </ul>
+                        <ul className="about--list2">
+                            <li>HTML&CSS</li>
+                            <li>Java</li>
+                        </ul>
+                    </div>
 
-                    <ul className="about--list2">
-                        <li>HTML&CSS</li>
-                        <li>Java</li>
-                    </ul>
-                </div>
+                    <p>Outside of CompSci work, I have always loved drawing and 
+                        illustration. Also I’m interested in research, language 
+                        learning, books, film making, photography and productivity.
+                    </p>
 
-                <p>Outside of CompSci work, I have always loved drawing and 
-                    illustration. Also I’m interested in research, language 
-                    learning, books, film making, photography and productivity.
-                </p>
-
-                <div>
-                    <a href={danielCV} download="Daniel Amado - CV"  className="about--cvbutton">
-                        <img src={imgDownload} alt="imagen descarga"/>
-                        <p>Download CV</p>
-                    </a>
+                    <div>
+                        <a href={danielCV} download="Daniel Amado - CV"  className="about--cvbutton">
+                            <img src={imgDownload} alt="imagen descarga"/>
+                            <p>Download CV</p>
+                        </a>
+                    </div>
                 </div>
 
                 
